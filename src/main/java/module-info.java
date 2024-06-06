9fc6e9c2dc7d1f86e829f7d1437bc2a9ea7dbf74
kaykyjuvenal <neguito.juvenal@gmail.com>
2024-06-05 21:04:37 -0300

    Atualização

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.view.rubbledumpsterrental {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.desktop;
+    requires java.naming;
+    requires java.management;
+    requires jdk.sctp;
+    requires org.apache.commons.csv;
+
+
+    opens com.view.rubbledumpsterrental to javafx.fxml;
+    exports com.view.rubbledumpsterrental;
+}
